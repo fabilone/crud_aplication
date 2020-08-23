c824c062fe8ea16622fd020e1ecff4da7ce76d8f
Fabilone Santos da Silva <fabilonesantos@hotmail.com>
2020-08-23 02:57:19 -0300

    Configurações de rotas iniciais

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+var handlebars = require('express-handlebars');
+var urlencodeParser = bodyParser.urlencoded({extended: false});
 
-app.use(express.static(__dirname));
+const flash = require("connect-flash");
+app.use(flash());
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname+"/src/index.js");
+//Routes de acesso
+app.get("/", function(req, res){
+  res.render('index');
+
 });
+
+//Template Engine
+app.engine("handlebars", handlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use(express.static(__dirname));
+app.use('/css', express.static('css'));
+app.use('/js', express.static('js'));
+app.use('/img', express.static('img'));
+
+/*app.get('/', function(req, res){
+  res.sendFile(__dirname+"/src/index.js");
+});*/
 
 var port = 3000;
 app.listen(port);
