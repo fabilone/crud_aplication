14d8f81fe52ac0ad874e1c5d23bbb06943236aef
Fabilone Santos da Silva <fabilonesantos@hotmail.com>
2020-08-26 03:47:45 -0300

    Config menu e organização de código

--- app.js
+++ app.js
@@ -12,14 +12,23 @@ app.use(function(req, res, next){
   next();
 });
 
+//Configurações da Base de Dados
+var pool = mysql.createPool({
+    
+  host: process.env.HOST || 'localhost',
+  user: process.env.USER || 'usuario_teste',
+  password: process.env.PASS || '5JUFBGHirkXbzZNw',
+  port: process.env.ACCSS || 3306,
+  database: "bd_rose_life" 
+
+});
+
 //Configurações de Arquivos
-const storage = multer.diskStorage({
-  
+const storage = multer.diskStorage({  
 	destination: function(req, file, cb){
 		cb(null, "uploads/");
 	},
 	filename: function(req, file, cb){
-
     pool.query("select * from crud_controlupload where codigo=1", function(err, results){
       if(err) res.sendStatus(500).send(err);
       else{
@@ -32,88 +41,31 @@ const storage = multer.diskStorage({
 
         pool.query("update crud_controlupload set last_upload=?, last_path_img=? ", [_id_user, caminho_user], function(err, retur){
           if(err) res.sendStatus(500).send(err);
-          else console.log("Registro de imagem feito com sucesso!");
-          
+          else console.log("Registro de imagem feito com sucesso!");          
         });
-        cb(null, caminho_user);
-          
-      }
-  
-    });
-		
+        cb(null, caminho_user);          
+      }  
+    });		
 	}
 });
 
+//Var Constantes
 const upload = multer({storage});
-
-//Configurações da Base de Dados
-var pool = mysql.createPool({
-    
-  host: process.env.HOST || 'localhost',
-  user: process.env.USER || 'usuario_teste',
-  password: process.env.PASS || '5JUFBGHirkXbzZNw',
-  port: process.env.ACCSS || 3306,
-  database: "bd_rose_life" 
-
-});
-
 const flash = require("connect-flash");
 app.use(flash());
+var context = {
+  texto: "<h6>Olá, bem-vido ao nosso sistema de cadastro</h6>",
+  texto_rodape: "<p>Todos os direitos reservados</p>"
+};
 
-var nav_bar = { mop1: "active", mop2: "", mop3:"", mop4: ""};
+//Template Engine
+app.engine("handlebars", handlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use(express.static(__dirname));
+app.use('/css', express.static('css'));
+app.use('/js', express.static('js'));
+app.use('/img', express.static('img'));
 
-//Routes de acesso
-app.get("/", function(req, res){
-  nav_bar = { mop1: "active", mop2: "", mop3:"", mop4: ""};
-  
-
-  pool.query("SELECT * FROM crud_linguagem WHERE status = 'true' ORDER BY name", function(err, results){
-    if(err) res.sendStatus(500).send(err);
-    else{
-      var msg_erro = [];
-      if(results.length == 0){ msg_erro[0] = "Ainda não existe nenhuma aplicação cadastrada no sistema."; }
-
-      res.render('index', {context, nav_bar, msg: msg_erro, linguagem: results});
-    }
-
-  });
-});
-
-app.get("/create", function(req, res){
-  nav_bar = { mop1: "", mop2: "active", mop3:"", mop4: ""}; 
-
-  pool.query("SELECT * FROM crud_linguagem ORDER BY name", function(err, results){
-    if(err) res.sendStatus(500).send(err);
-    else{
-
-      res.render('create-aplication', {nav_bar, linguagem: results});
-    }
-
-  });
-  
-});
-
-app.get("/list", function(req, res){
-  nav_bar = { mop1: "", mop2: "", mop3:"active", mop4: ""};
-
-  pool.query("SELECT * FROM crud_app ORDER BY name, app_language", function(err, results){
-    if(err){ res.sendStatus(500).send(err); }
-    else { res.render('read-aplication', {nav_bar, aplications: results}); }
-
-  });
-
-  
-});
-
-app.get("/list-linguagem", function(req, res){
-  nav_bar = { mop1: "", mop2: "", mop3:"active", mop4: ""};
-  res.render('read-aplication-linguagem', nav_bar);
-});
-
-app.get("/end-create-aplication", function(req, res){
-  res.render("error-control");
-
-});
 
 //Função para atualizar a liguagem
 function uplinguagem(name_lg){
@@ -124,28 +76,69 @@ function uplinguagem(name_lg){
   });
 }
 
+//Função para atualizar o menu
+function updateMenu(ativo){
+  var nav_bar = { op1: "", op2: "", op3: "", op4: "" };
+  if(ativo == 0) { nav_bar = { op1: "active", op2: "", op3: "", op4: "" }; }
+  if(ativo == 1) { nav_bar = { op1: "", op2: "active", op3: "", op4: "" }; }
+  if(ativo == 2) { nav_bar = { op1: "", op2: "", op3: "active", op4: "" }; }
+  if(ativo == 3) { nav_bar = { op1: "", op2: "", op3: "", op4: "active" }; }
+  return nav_bar; 
+}
 
-//Template Engine
-app.engine("handlebars", handlebars({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-app.use(express.static(__dirname));
-app.use('/css', express.static('css'));
-app.use('/js', express.static('js'));
-app.use('/img', express.static('img'));
 
-var context = {
-  texto: "<h6>Olá, bem-vido ao nosso sistema de cadastro</h6>",
-  texto_rodape: "<p>Todos os direitos reservados</p>"
-};
+//Routes de acesso
+//Home
+app.get("/", function(req, res){
+  pool.query("SELECT * FROM crud_linguagem WHERE status = 'true' ORDER BY name", function(err, results){
+    if(err) res.sendStatus(500).send(err);
+    else{
+      var msg_erro = [];
+      var nav_bar = updateMenu(0);  
+      if(results.length == 0){ msg_erro[0] = "Ainda não existe nenhuma aplicação cadastrada no sistema."; }
+      res.render('index', {context, nav_bar, msg: msg_erro, linguagem: results});
+    }
+  });
+});
+
+//Cadastro
+app.get("/create", function(req, res){
+  pool.query("SELECT * FROM crud_linguagem ORDER BY name", function(err, results){
+    if(err) res.sendStatus(500).send(err);
+    else{
+      var nav_bar = updateMenu(1); 
+      res.render('create-aplication', {nav_bar, linguagem: results});
+    }
+  });  
+});
+
+//Listar
+app.get("/list", function(req, res){
+  pool.query("SELECT * FROM crud_app ORDER BY app_language, name", function(err, results){
+    if(err){ res.sendStatus(500).send(err); }
+    else { var nav_bar = updateMenu(2); res.render('read-aplication', {nav_bar, aplications: results}); }
+  });  
+});
+
+app.get("/list-linguagem", function(req, res){
+  var nav_bar = updateMenu(2);
+  res.render('read-aplication-linguagem', nav_bar);
+});
+
+app.get("/end-create-aplication", function(req, res){
+  res.render("error-control");
+
+});
+
 
 //Rotas CRUD
 app.post("/search-aplication", function(req, res){
-  nav_bar = { mop1: "", mop2: "", mop3:"active", mop4: ""};
+  var nav_bar = updateMenu(2);
   res.render('search-aplication', nav_bar);
 });
 
 app.post("/info", function(req, res){
-  nav_bar = { mop1: "", mop2: "active", mop3:"", mop4: ""};
+  var nav_bar = updateMenu(1);
   res.render('detail', nav_bar);
 });
 
@@ -183,12 +176,10 @@ app.post("/register-aplication", upload.single('imgapp'), urlencodeParser, funct
 
 app.post("/select-list-linguagem", urlencodeParser, function(req, res){
 
-  nav_bar = { mop1: "", mop2: "", mop3:"active", mop4: ""};
-
   pool.query("SELECT * FROM crud_app WHERE  app_language = ? ORDER BY name", [req.body.selectlinguagem], function(err, results){
     if(err) res.sendStatus(500).send(err);
     else{
-
+      var nav_bar = updateMenu(2);
       res.render('read-aplication-linguagem', {nav_bar, lg_name: req.body.selectlinguagem, linguagem: results});
     }
 
